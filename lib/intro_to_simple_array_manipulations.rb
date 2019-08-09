1b4e31e37d749603b4554cee6b9e30a90e4432c7
Mark Bihag <markbihag330@gmail.com>
2019-08-09 04:53:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_push(array, string)
   array.push(string)
 end
 def using_unshift(array, string)
-  array.unshift(string)
+  array.unshif
 end
 def using_pop(array, string)
   array.pop(string)
